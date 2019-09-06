92c450a4656464acf083d107677ab6cef0836ce9
Ionut <ionut.oprea@code11.com>
2019-09-06 09:31:03 +0300

    Boilerplate setup

---
+++ micro-fe-boilerplate/src/react-app/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import singleSpaReact from 'single-spa-react'
+
+function domElementGetter() {
+  return document.getElementById("root")
+}
+
+const reactLifecycles = singleSpaReact({
+  React,
+  ReactDOM,
+  rootComponent: App,
+  domElementGetter,
+})
+
+export const bootstrap = [
+  reactLifecycles.bootstrap,
+]
+
+export const mount = [
+  reactLifecycles.mount,
+]
+
+export const unmount = [
+  reactLifecycles.unmount,
+]
